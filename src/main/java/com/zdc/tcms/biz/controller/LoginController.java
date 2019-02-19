d206e06a038a3cfc573602e6cb01602c66cba891
zhangdc <111.com>
2019-02-19 22:41:10 +0800

    登陆页面实现

---
+++ src/main/java/com/zdc/tcms/biz/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.zdc.tcms.biz.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class LoginController {
+
+    @RequestMapping(value = "/userLogin")
+    public ModelAndView userLogin(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("login");
+        return mv;
+    }
+}
